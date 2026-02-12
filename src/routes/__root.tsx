386af28bb0ca336bdb2252c6c2a1bc68a96dd128
Délita Makanda <delitamakanda@users.noreply.github.com>
2026-02-12 10:31:44 +0100

    Add role attribute to body element

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -48,7 +48,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body>
+      <body role="main">
         <Providers>
           <Header />
 
